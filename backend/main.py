4b1b7b8a739661a7767db7a28d128a148c658d0c
haidang <dang.vohaidang@hcmut.edu.vn>
2023-11-09 23:15:00 +0700

    1

--- backend/main.py
+++ backend/main.py
@@ -1,87 +1,80 @@
-from flask import Flask, request
-from flask import render_template
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Request
+from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
 import settings
 import utils
 import numpy as np
 import cv2
+import uvicorn
 import predictions as pred
+import database
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from security import generate_token, validate_token
+from sqlalchemy.orm import Session
 
-app = Flask(__name__)
-app.secret_key = 'document_scanner_app'
+# docscan = utils.DocumentScan()
+app = FastAPI()
+template = Jinja2Templates(directory='templates')
+# app.secret_key = 'scanapp'
 
-docscan = utils.DocumentScan()
+@app.get('/')
+def home(request: Request):
+    return {"message": "Hello World"}
 
+class UserBase(BaseModel):
+    username: str
+    email: str
 
-@app.route('/', methods=['GET', 'POST'])
-def scandoc():
-    
-    if request.method == 'POST':
-        file = request.files['image_name']
-        upload_image_path = utils.save_upload_image(file)
-        print('Image saved in = ', upload_image_path)
-        # predict the coordination of the document
-        four_points, size = docscan.document_scanner(upload_image_path)
-        print(four_points, size)
-        if four_points is None:
-            message = 'UNABLE TO LOCATE THE COORDIANATES OF DOCUMENT: points displayed are random'
-            points = [
-                {'x': 10, 'y': 10},
-                {'x': 120, 'y': 10},
-                {'x': 120, 'y': 120},
-                {'x': 10, 'y': 120}
-            ]
-            return render_template('scanner.html',
-                                   points=points,
-                                   fileupload=True,
-                                   message=message)
-            
-        else:
-            points = utils.array_to_json_format(four_points)
-            message = 'Located the Cooridinates of Document using OpenCV'
-            return render_template('scanner.html',
-                                   points=points,
-                                   fileupload=True,
-                                   message=message)
-            
-        
-        return render_template('scanner.html')
-    
-    return render_template('scanner.html')
-
-@app.route('/transform', methods=['POST'])
-def transform():
+@app.post('/signup')
+def signup(user: models.User):
     try:
-        points = request.json['data']
-        array = np.array(points)
-        magic_color = docscan.calibrate_to_original_size(array)
-        #utils.save_image(magic_color,'magic_color.jpg')
-        filename = 'magic_color.jpg'
-        magic_image_path = settings.join_path(settings.MEDIA_DIR, filename)
-        cv2.imwrite(magic_image_path, magic_color)
-        
-        return 'sucess'
-    except:
-        return 'fail'
-        
-    
-@app.route('/prediction')
-def prediction():
-    # load the wrap image
-    wrap_image_filepath = settings.join_path(settings.MEDIA_DIR, 'magic_color.jpg') 
-    image = cv2.imread(wrap_image_filepath)
-    image_bb, results = pred.getPredictions(image)
-    
-    bb_filename = settings.join_path(settings.MEDIA_DIR, 'bounding_box.jpg') 
-    cv2.imwrite(bb_filename, image_bb)
-    
-    return render_template('predictions.html', results=results)
+        if any(field is None for field in [user.email, user.username, user.password]):
+            raise HTTPException(status_code=400, detail="Information don't enough!")
+        userDb = database.getOne({"email": user.email})
+        if userDb is None:
+            id = database.create(user)
+            if id is None:
+                raise HTTPException(status_code=400, detail="Sign Up don't succeed!")
+            user = database.getOne({"_id": id})
+            token = generate_token(user['email'])
+            return {
+                "user": user,
+                "token": token
+            }
+        raise HTTPException(status_code=400, detail="User have exist")
+    except ValueError as err:
+        raise HTTPException(status_code=500, detail=err)
+
+def verify(user: models.User, userDb: models.User):
+    if userDb is None:
+        return ["User not found!", False]
+    if user.email != userDb.get('email'):
+        return ["Wrong email!", False]
+    elif user.password != userDb.get('password'):
+        return ["Wrong password!", False]
+    return ["", True]
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.post('/signin')
+def signin(user: models.User):
+    try:
+        userDb = database.getOne({"email": user.email})
+        check = verify(user, userDb)
+        if check[1] == True:
+            token = generate_token(user.email)
+            return {
+                "user": userDb,
+                "token": token
+            }
+        raise HTTPException(status_code=400, detail=check[0])
+    except ValueError as err:
+        raise HTTPException(status_code=500, detail=err)
 
-
+@app.get("/")
+def index():
+    return {"message": "Welcome"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    uvicorn.run(app, host='localhost', port=8000)
